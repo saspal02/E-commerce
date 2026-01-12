cf79c7cc07fedb2d0c93c482607819d46cfd0d0e
Saswat Pal <saswat2001@gmail.com>
2026-01-12 22:44:30 +0530

    feat(order, payment): implement order and payment DTOs

---
+++ Backend/src/main/java/com/ecommerce/controller/OrderController.java
@@ -0,0 +1,38 @@
+package com.ecommerce.controller;
+
+import com.ecommerce.payload.OrderDTO;
+import com.ecommerce.payload.OrderRequestDTO;
+import com.ecommerce.util.AuthUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> orderProducts(@PathVariable String paymentMethod,
+                                                  @RequestBody OrderRequestDTO orderRequestDTO) {
+        String emailId = authUtil.loggedInEmail();
+        OrderDTO order = orderService.placeOrder(
+                emailId,
+                orderRequestDTO.getAddressId(),
+                paymentMethod,
+                orderRequestDTO.getPgName(),
+                orderRequestDTO.getPgPaymentId(),
+                orderRequestDTO.getPgStatus(),
+                orderRequestDTO.getPgResponseMessage()
+        );
+        return new ResponseEntity<>(order, HttpStatus.CREATED);
+
+    }
+
+}
---
+++ Backend/src/main/java/com/ecommerce/model/Payment.java
@@ -0,0 +1,42 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @OneToOne(mappedBy = "payment", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    private Order order;
+
+    @NotBlank
+    @Size(min = 4, message = "Payment method must be at least 4 characters")
+    private String paymentMethod;
+
+    private String pgPaymentId;
+    private String pgStatus;
+    private String pgResponseMessage;
+    private String pgName;
+
+    public Payment(long paymentId, String pgPaymentId, String pgStatus, String pgResponseMessage, String pgName) {
+        this.paymentId = paymentId;
+        this.pgPaymentId = pgPaymentId;
+        this.pgStatus = pgStatus;
+        this.pgResponseMessage = pgResponseMessage;
+        this.pgName = pgName;
+    }
+}
+
+

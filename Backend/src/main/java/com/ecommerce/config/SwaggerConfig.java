8a2385bba6e67e90c233ff6d66dba40d35481170
Saswat Pal <saswat2001@gmail.com>
2026-01-16 22:46:02 +0530

    feat: add Swagger/OpenAPI documentation

--- Backend/src/main/java/com/ecommerce/config/SwaggerConfig.java
+++ Backend/src/main/java/com/ecommerce/config/SwaggerConfig.java
@@ -2,11 +2,14 @@ package com.ecommerce.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class SwaggerConfig {
 
@@ -22,9 +25,14 @@ public class SwaggerConfig {
                 .addList("Bearer Authentication");
 
         return new OpenAPI()
+                .info(new Info()
+                        .title("E-Commerce API")
+                        .version("1.0")
+                        .description("API documentation for the E-Commerce application")
+                )
                 .components(new Components()
-                .addSecuritySchemes("Bearer Authentication", bearerScheme));
-
-        )
+                        .addSecuritySchemes("Bearer Authentication", bearerScheme)
+                )
+                .addSecurityItem(bearerRequirement);
     }
 }

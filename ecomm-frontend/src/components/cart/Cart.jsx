dea0a6a34c18acd7a15fe60dddc99abcc503bf64
Saswat Pal <saswat2001@gmail.com>
2026-02-24 22:53:13 +0530

    chore: minor UI spacing and formatting fixes

--- ecomm-frontend/src/components/cart/Cart.jsx
+++ ecomm-frontend/src/components/cart/Cart.jsx
@@ -1,5 +1,17 @@
+import { MdArrowBack, MdShoppingCart } from "react-icons/md";
+
 const Cart = () => {
-  return <h1>Cart page</h1>;
+  return (
+    <div className="lg:px-14 sm:px-8 px-4 py-10">
+      <div className="flex flex-col items-center mb-12">
+        <h1 className="text-4xl text-gray-900 flex items-center gap-3">
+          <MdShoppingCart size={36} className="text-grey-700" />
+          Your Cart
+        </h1>
+        <p>All your selected items</p>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
